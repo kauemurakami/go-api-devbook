b782f1235fb259bebd8c035b54ef7f8b661ff039
Kauê <kauetmurakami@gmail.com>
2024-05-19 03:55:43 -0300

    try like use

--- app/internal/users/users_routes.go
+++ app/internal/users/users_routes.go
@@ -9,6 +9,7 @@ import (
 func SetupUserRoutes(router *mux.Router) {
 	router.HandleFunc("/users", CreateUser).Methods(http.MethodPost)
 	router.HandleFunc("/users", GetUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{user}", GetUsersByNickOrName).Methods(http.MethodGet)
 	router.HandleFunc("/users/{id}", GetUser).Methods(http.MethodGet)
 	router.HandleFunc("/users/{id}", UpdateUser).Methods(http.MethodPut)
 	router.HandleFunc("/users/{id}", DeleteUser).Methods(http.MethodDelete)
