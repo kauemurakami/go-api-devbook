c31e61be32c677d86d07eec7a7462732ba2c7411
Kauê <kauetmurakami@gmail.com>
2024-05-19 03:29:29 -0300

    validação de usuario

--- app/models/user.go
+++ app/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,4 +15,36 @@ type User struct {
 	Email     string    `json:"email,omityempty"`
 	Pass      string    `json:"pass,omityempty"`
 	CreatedAt time.Time `json:"created_at,omityempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+	user.formatter()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("O nome deve ser inserido")
+	}
+	if user.Email == "" {
+		return errors.New("O email deve ser inserido")
+	}
+	if user.Nick == "" {
+		return errors.New("O nick deve ser inserido")
+	}
+	if user.Pass == "" {
+		return errors.New("O password deve ser inserido")
+	}
+
+	return nil
+}
+
+func (user *User) formatter() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Pass = strings.TrimSpace(user.Pass)
 }
--- go.mod
+++ go.mod
@@ -5,15 +5,14 @@ go 1.22.3
 require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/jackc/pgx/v5 v5.5.5
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx v3.6.2+incompatible // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
