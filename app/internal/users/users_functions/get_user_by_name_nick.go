381e699891cb2b7e17e96a5e10dbbb5971a404e6
Kauê <kauetmurakami@gmail.com>
2024-05-20 19:34:02 -0300

    get by nick or name with LIKE

--- app/internal/users/users_functions/get_user_by_name_nick.go
+++ app/internal/users/users_functions/get_user_by_name_nick.go
@@ -5,6 +5,7 @@ import (
 	responses "api-social-media/app/core/helpers/response"
 	"api-social-media/app/models"
 	"context"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -13,35 +14,44 @@ func GetUsersByNickOrName(w http.ResponseWriter, r *http.Request) {
 	conn := db.SetupDB()
 	defer conn.Close(context.Background())
 
-	query := "SELECT * FROM users lower(name) LIKE $1 OR lower(nick) LIKE $1"
-	var users []models.User
-	nameOrNick := "%" + strings.ToLower(r.URL.Query().Get("user")) + "%"
-	rows, err := conn.Query(context.Background(),
-		query,
-		nameOrNick,
-	)
+	// Corrigir a query SQL
+	query := "SELECT * FROM users WHERE lower(name) LIKE $1 OR lower(nick) LIKE $1"
+
+	// Extrair o parâmetro de consulta 'user'
+	user_param := r.URL.Query().Get("user")
+	if user_param == "" {
+		responses.Err(w, http.StatusBadRequest, fmt.Errorf("Parâmetro de consulta 'user' está faltando"))
+		return
+	}
+
+	nameOrNick := "%" + strings.ToLower(user_param) + "%"
+
+	rows, err := conn.Query(context.Background(), query, nameOrNick)
 	if err != nil {
-		responses.Err(w, http.StatusBadRequest, err)
+		responses.Err(w, http.StatusBadRequest, fmt.Errorf("Erro ao executar query: %v", err))
+		return
 	}
 	defer rows.Close()
 
+	var users []models.User
 	for rows.Next() {
 		var user models.User
 		if err := rows.Scan(&user.ID, &user.Name, &user.Nick, &user.Email, &user.Pass, &user.CreatedAt); err != nil {
-			responses.Err(w, http.StatusBadRequest, err)
-
+			responses.Err(w, http.StatusBadRequest, fmt.Errorf("Erro ao escanear linha: %v", err))
+			return
 		}
 		users = append(users, user)
 	}
 
 	if err := rows.Err(); err != nil {
-		responses.Err(w, http.StatusBadRequest, err)
-
-	}
-	responses.JSON(w, http.StatusOK, users)
-	if err != nil {
-		responses.Err(w, http.StatusInternalServerError, err)
+		responses.Err(w, http.StatusBadRequest, fmt.Errorf("Erro nas linhas: %v", err))
 		return
 	}
 
+	// Se não houver usuários encontrados, retornar um array vazio de usuários
+	if len(users) == 0 {
+		users = []models.User{}
+	}
+
+	responses.JSON(w, http.StatusOK, users)
 }
