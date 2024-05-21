03db7d3084d3b39bf197b39819fde9ff14fc8d27
Kauê <kauetmurakami@gmail.com>
2024-05-21 20:56:02 -0300

    add follow and unfollow

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"api-social-media/app/internal/auth"
+	"api-social-media/app/internal/followers"
 	"api-social-media/app/internal/users"
 
 	"github.com/gorilla/mux"
@@ -12,5 +13,6 @@ func SetupAppRoutes() *mux.Router {
 	router := mux.NewRouter()
 	auth.SetupAuthRoutes(router)
 	users.SetupUserRoutes(router)
+	followers.SetupFollowersRoutes(router)
 	return router
 }
